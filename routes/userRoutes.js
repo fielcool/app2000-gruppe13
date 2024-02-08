c865c77b1bd89d635de4c4ac50ab210296a57a68
fielcool <115153391+fielcool@users.noreply.github.com>
2024-02-08 02:52:12 +0100

    Revert "	deleted:    db.js"
    This reverts commit 5f0675fcc7ea5c7899323161ca5027514af91091.

---
+++ routes/userRoutes.js
@@ -0,0 +1,41 @@
+// routes/userRoutes.js
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+router.post('/api/createUser', async (req, res) => {
+  try {
+    
+    const { Navn , Email, Passord, Organisasjon, Stillingstittel } = req.body;
+
+    // Check if the user with the provided email already exists
+    const existingUser = await User.findOne({ Email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User with this email already exists' });
+    }
+
+    // Hash the password before saving it to the database
+    const hashedPassword = await bcrypt.hash(Passord, 10);
+
+    // Create a new user
+    const newUser = new User({
+      Navn,
+      Organisasjon,
+      Stillingstittel,
+      Email,
+      Passord: hashedPassword,
+      // Add other fields as needed
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
