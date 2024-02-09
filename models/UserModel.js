41f13615f6c63ef4bb98c10867ac04601ab616c8
fielcool <115153391+fielcool@users.noreply.github.com>
2024-02-09 07:32:20 +0100

    modified:   models/UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -9,24 +9,6 @@ const userSchema = new mongoose.Schema({
   passord: { type: String, required: true }
 });
 
-// Adding a pre-save hook to hash the password before saving to the database
-userSchema.pre('save', async function(next) {
-  try {
-    // Only hash the password if it has been modified or is new
-    if (!this.isModified('passord')) {
-      return next();
-    }
-
-    // Hash the password with a salt round of 10
-    const hashedPassword = await bcrypt.hash(this.passord, 10);
-    this.passord = hashedPassword;
-
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-});
-
 // Adding a method to compare passwords during login
 userSchema.methods.comparePassword = async function(candidatePassword) {
   try {
