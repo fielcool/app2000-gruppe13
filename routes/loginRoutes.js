0560affc8b56dfd6b714d5cd3b1f43867943ea6b
fielcool <115153391+fielcool@users.noreply.github.com>
2024-02-12 01:53:00 +0100

    modified:   routes/loginRoutes.js

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -16,25 +16,18 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ error: 'Invalid credentials mail' });
         }
 
-        // Logging for debugging
-        console.log('Entered Password:', passord);
-        console.log('Stored Hashed Password:', user.passord);
-
         // Compare the entered password with the hashed password in the database
         const isPasswordValid = await bcrypt.compare(passord, user.passord);
-
-        // Logging for debugging
-        console.log('Is password valid?', isPasswordValid);
 
         if (!isPasswordValid) {
             console.log('Invalid credentials: Password mismatch');
             return res.status(401).json({ error: 'Invalid credentials password' });
         }
 
-        // If the password is valid, you can generate a token and send it to the client for authentication
-        // For simplicity, let's just send a success message for now
+        // If the password is valid, generate a token and send it in the response
+        const token = generateToken(user);
         console.log('Login successful');
-        res.status(200).json({ message: 'Login successful' });
+        res.status(200).json({ token });
     } catch (error) {
         console.error('Error logging in:', error);
         res.status(500).json({ error: 'Internal Server Error', details: error.message, stack: error.stack });
