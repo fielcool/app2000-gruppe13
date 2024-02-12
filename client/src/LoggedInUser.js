794e0a586597de5f7629ea406d5e35086a889f34
fielcool <115153391+fielcool@users.noreply.github.com>
2024-02-12 01:39:33 +0100

    modified:   client/src/LoggedInUser.js 	modified:   server.js

--- client/src/LoggedInUser.js
+++ client/src/LoggedInUser.js
@@ -4,21 +4,11 @@ import { Card } from "react-bootstrap";
 const LoggedInUser = () => {
   return (
     <div className="main">
-      <Card style={{ width: '88rem' }}>
+      <Card style={{ width: '44rem' }}>
         <Card.Img variant="top" src="https://ninjatables.com/wp-content/uploads/2023/07/Best-Data-Comparison-Charts.jpg" />
-        <Card.Body>
-          <Card.Title>Logged In User</Card.Title>
-          <Card.Text>
-            Welcome! This is your profile. Add more content as needed.
-          </Card.Text>
-          {/* Button to delete the user */}
-          <Button variant="danger" onClick={handleDeleteUser}>
-            Slett min bruker
-          </Button>
-        </Card.Body>
       </Card>
     </div>
   );
-}
+};
 
 export default LoggedInUser;
--- server.js
+++ server.js
@@ -25,13 +25,7 @@ app.use(cookieParser());
 app.use(morgan("tiny"));
 
 // Token verification middleware for protected routes
-<<<<<<< HEAD
-app.get('/api/loggedInUser', verifyToken, (req, res) => {
-  res.json({ message: 'Welcome to the protected route, ' + req.user.username + '!', email: req.user.email });
-});
-=======
 app.use('/api/loggedInUser', verifyToken);
->>>>>>> parent of 0136624 (	modified:   client/src/LoggedInUser.js)
 
 // Your existing routes
 app.use('/api', loginRoutes, userRoutes);
