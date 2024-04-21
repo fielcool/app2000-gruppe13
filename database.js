d241c670fecc00efb02d742b0089b00578aedb0d
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-22 01:56:30 +0200

    modified:   database.js 	modified:   routes/userRoutes.js

--- database.js
+++ database.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 // Function to establish a database connection
-function connectToDatabase(uri, options) {
+function connectToDatabase(uri, options, dbName) {
+    // Create a new database connection instance
     const connection = mongoose.createConnection(uri, options);
     
     // Event handling for the database connection
-    connection.on('error', console.error.bind(console, 'MongoDB Connection Error:'));
+    connection.on('error', console.error.bind(console, `MongoDB Connection Error for ${dbName}:`));
     connection.once('open', () => {
-        console.log('Database connected');
+        console.log(`Database connected for ${dbName} at ${uri}`);
     });
 
     return connection;
@@ -20,10 +21,10 @@ const options = {
 };
 
 // Connect to the 'brukere' database
-const connection1 = connectToDatabase(process.env.MONGODB_URI, options);
+const connection1 = connectToDatabase(process.env.MONGODB_URI, options, 'brukere');
 
 // Connect to the 'bigfive' database
-const connection2 = connectToDatabase(process.env.MONGODB_URI_2, options);
+const connection2 = connectToDatabase(process.env.MONGODB_URI_2, options, 'bigfive');
 
 // Export the database connections
 module.exports = { connection1, connection2 };
