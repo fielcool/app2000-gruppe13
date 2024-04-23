e543a158718c8662947d6e02f256f6818b3cabf1
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-23 23:25:29 +0200

    modified:   client/src/CreateUserForm.jsx 	modified:   routes/savePersonalityTestIdRoute.js

--- routes/savePersonalityTestIdRoute.js
+++ routes/savePersonalityTestIdRoute.js
@@ -1,21 +1,21 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../models/User");
+const User = require('../models/UserModel');
+const { protect } = require('../middleware');
 
-router.post("/savePersonalityTestId", async (req, res) => {
-  const { testId } = req.body;
-  const userId = req.user.userId; // Assuming you have user ID in the authentication token
-
+// Route to update the user's testId
+router.put('/updateTestId', protect, async (req, res) => {
   try {
-    // Find the user by ID and update the personalityTestId field
-    await User.findByIdAndUpdate(userId, { personalityTestId: testId });
+    const { testId } = req.body;
+    const userId = req.user._id; // Assuming the user ID is available in the request object
 
-    // Optionally, update the organization's users as well if needed
+    // Update the user's testId
+    await User.findByIdAndUpdate(userId, { testId });
 
-    res.status(200).send("Personality test ID saved successfully");
+    res.status(200).json({ message: 'TestId updated successfully' });
   } catch (error) {
-    console.error("Error saving personality test ID:", error);
-    res.status(500).send("Internal Server Error");
+    console.error('Error updating testId:', error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 });
 
