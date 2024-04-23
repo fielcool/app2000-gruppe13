2257bd13b84fcd4f1171c71b032d3dc428bb0398
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-23 23:42:49 +0200

    modified:   routes/savePersonalityTestIdRoute.js

--- routes/savePersonalityTestIdRoute.js
+++ routes/savePersonalityTestIdRoute.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware');
 const User = require('../models/UserModel');
+const { protect } = require('../middleware');
 
-// Update user's testId
+// Route to update the user's testId
 router.put('/updateTestId', protect, async (req, res) => {
-    try {
-      const { testId } = req.body;
-      const userId = req.user._id;
-      console.log('Received Test ID:', testId);
-      console.log('User ID:', userId);
-  
-      // Update the user's testId
-      const updatedUser = await User.findByIdAndUpdate(userId, { testId }, { new: true });
-  
-      if (!updatedUser) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-  
-      res.status(200).json({ message: 'TestId updated successfully', user: updatedUser });
-    } catch (error) {
-      console.error('Error updating testId:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
+  try {
+    const { testId } = req.body;
+    const userId = req.user._id; // Assuming the user ID is available in the request object
+
+    console.log('Received testId:', testId);
+    console.log('Updating testId for user:', userId);
+
+    // Update the user's testId
+    await User.findByIdAndUpdate(userId, { testId });
+
+    console.log('TestId updated successfully');
+
+    res.status(200).json({ message: 'TestId updated successfully' });
+  } catch (error) {
+    console.error('Error updating testId:', error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+  }
+});
+
+module.exports = router;
