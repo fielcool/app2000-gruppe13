3630ab835be4d6dfcc39241d2240c0c6c521ea4e
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-25 00:46:19 +0200

    modified:   models/TestResult.js 	modified:   routes/crossReferenceRoutes.js

--- models/TestResult.js
+++ models/TestResult.js
@@ -1,9 +1,11 @@
-// models/TestResult.js
-
 const mongoose = require('mongoose');
 const { connection2 } = require('../database');
 
 const testResultSchema = new mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
   testId: {
     type: String,
     required: true
@@ -14,28 +16,45 @@ const testResultSchema = new mongoose.Schema({
   },
   invalid: {
     type: Boolean,
-    default: false
+    required: true
   },
-  answers: [
-    {
-      questionID: {
-        type: String,
-        required: true
-      },
-      score: {
-        type: Number,
-        required: true
-      },
-      domain: {
-        type: String,
-        required: true
-      },
-      facet: {
-        type: Number,
-        required: true
+  answers: {
+    type: [
+      {
+        questionID: {
+          type: String,
+          required: true
+        },
+        score: {
+          type: Number,
+          required: true
+        },
+        domain: {
+          type: String,
+          required: true
+        },
+        facet: {
+          type: Number,
+          required: true
+        }
       }
+    ],
+    required: true,
+    validate: {
+      validator: function(v) {
+        return v.length === 120; 
+      },
+      message: props => `${props.value} is not a valid length for answers array.`
     }
-  ]
+  },
+  timeElapsed: {
+    type: Int32,
+    required: true
+  },
+  dateStamp: {
+    type: mongoose.Schema.Types.Double,
+    required: true
+  }
 }, { timestamps: true });
 
 const TestResult = connection2.model('TestResults', testResultSchema, 'results');
--- routes/crossReferenceRoutes.js
+++ routes/crossReferenceRoutes.js
@@ -12,44 +12,42 @@ router.get('/pieChart', verifyToken, async (req, res) => {
 
     // Retrieve data from the 'brukere' database (connection1)
     const User = connection1.model('User', UserModel);
-// Log the TestResult model
-console.log(TestResult);
 
-// Log the collection name
-console.log(TestResult.collection.name);
+    // Retrieve data from the 'results' collection (connection2)
+    const TestResultModel = connection2.model('TestResult', TestResult);
 
-     // Perform aggregation
-     const aggregateScores = await User.aggregate([
-        {
-          $match: { organisasjon: organisasjon } // Filter by organization
-        },
-        {
-          $lookup: {
-            from: TestResult.collection.name,
-            localField: "resultatId",
-            foreignField: "_id",
-            as: "testResults"
-          }
-        },
-        {
-          $unwind: "$testResults" // Unwind the array of test results
-        },
-        {
-          $group: {
-            _id: "$testResults.domain",
-            totalScore: { $sum: "$testResults.score" }
-          }
+    // Perform aggregation
+    const aggregateScores = await User.aggregate([
+      {
+        $match: { organisasjon: organisasjon } // Filter by organization
+      },
+      {
+        $lookup: {
+          from: TestResultModel.collection.name,
+          localField: "resultatId",
+          foreignField: "_id",
+          as: "testResults"
         }
-      ]);
-  
-      // Log the aggregated scores for debugging
-      console.log('Aggregate Scores:', aggregateScores);
-  
-      // Format aggregated scores into data suitable for a pie chart
-      const pieChartData = aggregateScores.map(score => ({
-        domain: score._id,
-        score: score.totalScore
-      }));
+      },
+      {
+        $unwind: "$testResults" // Unwind the array of test results
+      },
+      {
+        $group: {
+          _id: "$testResults.domain",
+          totalScore: { $sum: "$testResults.score" }
+        }
+      }
+    ]);
+
+    // Log the aggregated scores for debugging
+    console.log('Aggregate Scores:', aggregateScores);
+
+    // Format aggregated scores into data suitable for a pie chart
+    const pieChartData = aggregateScores.map(score => ({
+      domain: score._id,
+      score: score.totalScore
+    }));
 
     // Respond with the formatted pie chart data
     res.status(200).json(pieChartData);
