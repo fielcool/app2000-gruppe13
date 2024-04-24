9302942daa19b052b74abf30024c67ac8313eb52
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-24 21:19:54 +0200

    modified:   routes/crossReferenceRoutes.js

--- routes/crossReferenceRoutes.js
+++ routes/crossReferenceRoutes.js
@@ -19,7 +19,7 @@ router.get('/pieChart', verifyToken, async (req, res) => {
           totalScore: { $sum: '$score' } // Aggregate scores for each domain
         }
       }
-    ]).option({ maxTimeMS: 20000 }); // Increase timeout to 20 seconds (20000 milliseconds)
+    ]).exec({ maxTimeMS: 20000 }); // Increase timeout to 20 seconds (20000 milliseconds)
 
     // Format aggregated scores into data suitable for a pie chart
     const pieChartData = aggregateScores.map(score => ({
