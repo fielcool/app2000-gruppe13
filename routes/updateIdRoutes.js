b67d01fc4369ae0d96e38a52e50900b01d821ba8
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-25 00:11:53 +0200

    modified:   routes/updateIdRoutes.js

--- routes/updateIdRoutes.js
+++ routes/updateIdRoutes.js
@@ -6,21 +6,21 @@ const User = require('../models/UserModel');
 // Route to update the user's testId
 router.put('/updateTestId', verifyToken, async (req, res) => {
   try {
-    const { testId } = req.body;
+    const { resultatId } = req.body;
     const userId = req.user.userId; // Assuming the user ID is available in the request object
 
-    console.log('Received testId:', testId);
+    console.log('Received testId:', resultatId);
     console.log('Updating testId for user:', userId);
 
     // Update the user's testId
-    await User.findByIdAndUpdate(userId, { testId },
+    await User.findByIdAndUpdate(userId, { resultatId },
     {new: true});
 
     console.log('TestId updated successfully');
 
-    res.status(200).json({ message: 'TestId updated successfully' });
+    res.status(200).json({ message: 'resultatId updated successfully' });
   } catch (error) {
-    console.error('Error updating testId:', error);
+    console.error('Error updating resultatId:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 });
