da96c5a8bdbdfd6509ab68c2d9447065466f1cff
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 18:52:53 +0200

    modified:   client/src/Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-white">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <a href="/">
           <img src="https://sveeee.vercel.app/_nuxt/img/7bc354c.svg" alt="Logo" />
