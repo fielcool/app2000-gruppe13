e4094fda4279b671bba3dbccf5f4b28e73c4ea4a
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 22:58:48 +0200

    modified:   client/src/Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { useAuth } from './context/AuthContext'; // Import the useAuth hook
 import brainImage from './images/logo.svg'; // Import the image
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
-  console.log('brainImage:', brainImage); // Log the brainImage variable
+  const { authToken, logout } = useAuth(); // Destructure authToken and logout from the useAuth hook
+
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <Link to="/">
           <img src={brainImage} alt="Logo" className="navbar-brand" style={{ maxHeight: '50px', marginRight: '25%' }} />
         </Link>
+        {authToken && ( // Render the logout button only if authToken is truthy (i.e., user is logged in)
+          <Button variant="danger" onClick={logout}>
+            Logg ut
+          </Button>
+        )}
       </div>
     </nav>
   );
