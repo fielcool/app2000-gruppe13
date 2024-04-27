711dfd5b3d77bedc63a6eb9f6dd86ab753f561be
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 20:51:14 +0200

    modified:   client/src/Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import brainImage from './images/logo.svg'; // Import the image
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
+  console.log('brainImage:', brainImage); // Log the brainImage variable
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link to="/">
-        <img src={brainImage} alt="Logo" className="navbar-brand" style={{ maxHeight: '50px', marginRight: '10px' }} />
+          <img src={brainImage} alt="Logo" className="navbar-brand" style={{ maxHeight: '50px', marginRight: '10px' }} />
         </Link>
       </div>
     </nav>
