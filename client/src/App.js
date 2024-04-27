74acdce354895a8b4a527c067a1824557d77ebe9
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 18:10:12 +0200

    modified:   client/src/App.css 	modified:   client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import OrgOverview from './OrgOverview';
-import LoginPage from './LoginPage';
-import SignupPage from './SignupPage';
+import loginUser from './LoginUser';
+import UserInfoFields from './UserInfoFields';
 
 const App = () => {
   return (
@@ -11,8 +11,8 @@ const App = () => {
       <Header showLogin showSignup />
       <Switch>
         <Route exact path="/" component={OrgOverview} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/signup" component={SignupPage} />
+        <Route path="/login" component={loginUser} />
+        <Route path="/signup" component={UserInfoFields} />
       </Switch>
     </Router>
   );
