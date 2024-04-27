d1f1d57b7136264fa072da065abb493ad3c23cbe
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 19:51:05 +0200

    modified:   client/src/Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg">
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container-logo">
       <img src={logo} alt="Logo" className="navbar-brand" />
       </div>
