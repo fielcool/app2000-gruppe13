a8073b8d4cff91beb54d4286ecb49f2d7a43beb8
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 18:28:31 +0200

    modified:   client/src/Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link to="/">
-          <img src="/images/logo.svg" alt="Logo" />
+          <img src="/images/logo" alt="Logo" />
         </Link>
       </div>
     </nav>
