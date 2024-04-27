d3bd064693564707fb74fc3cc819a7ccdd1a7137
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 20:09:37 +0200

    modified:   client/src/Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from './images/logo.svg'; // Import the image
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg">
       <div className="container-logo">
         <img src={logo} alt="Logo" /> {/* Use the imported image */}
       </div>
