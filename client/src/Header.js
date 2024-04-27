502bdfc5db599e7d9aba4d7fa1fab558cf6f6ff5
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 20:25:07 +0200

    modified:   client/src/Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
       <a href="/">
-      <img src={brainImage} alt="Logo" className="navbar-brand" style={{ maxHeight: '50px', marginleft: '25%' }} />
+      <img src={brainImage} alt="Logo" className="navbar-brand" style={{ maxHeight: '50px', marginLeft: '10px' }} />
       </a>
       </div>
     </nav>
