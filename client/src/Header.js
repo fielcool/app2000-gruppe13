421d6ee701980b84508690886b8b61355c9ed3f4
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 19:15:28 +0200

    modified:   client/src/Header.js 	deleted:    client/src/images/logo.png 	renamed:    client/src/images/logo.svg -> client/src/logo.svg

--- client/src/Header.js
+++ client/src/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link to="/">
-          <img src="/images/logo.png" alt="Logo" />
+          <img src="./logo.svg" alt="Logo" />
         </Link>
       </div>
     </nav>
