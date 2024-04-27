4d354fe7644cc3ba381f9ddfc173658b8b3021ae
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-28 00:03:28 +0200

    modified:   client/src/App.css 	renamed:    client/src/CreateUserForm.jsx -> client/src/CreateUserForm.js 	modified:   client/src/LoggedInUser.js 	modified:   client/src/PersonalityTestIdForm.js

--- client/src/PersonalityTestIdForm.js
+++ client/src/PersonalityTestIdForm.js
@@ -3,13 +3,24 @@ import axios from "axios";
 
 function PersonalityTestIdForm({ authToken }) {
   const [resultatId, setTestId] = useState("");
+  const [isValidFormat, setIsValidFormat] = useState(true); // State to track if the format is valid
 
   const handleTestIdChange = (e) => {
-    setTestId(e.target.value);
+    const inputValue = e.target.value;
+    setTestId(inputValue);
+
+    // Validate the input format using regular expressions
+    const isValid = /^[0-9a-fA-F]{24}$/.test(inputValue);
+    setIsValidFormat(isValid);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!isValidFormat) {
+      alert("Invalid ObjectId format!"); // Display an alert if the format is invalid
+      return;
+    }
+
     try {
       console.log("Sending request to save personality test ID...");
       await axios.put(
@@ -29,13 +40,13 @@ function PersonalityTestIdForm({ authToken }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}className="id-input">
+    <form onSubmit={handleSubmit} className="id-input">
       <input 
         type="text"
         value={resultatId}
         onChange={handleTestIdChange}
         placeholder="Lim inn ID fra personlighetstesten"
-        className="wider-input"
+        className={`wider-input ${isValidFormat ? '' : 'invalid-format'}`} // Apply different class based on format validity
       />
       <button type="submit" className="btn btn-info btn-md fp-button shadow-custom">Lagre test-ID</button>
     </form>
