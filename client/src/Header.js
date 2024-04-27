3b074ac23f9420a07f85afb0fb7ddbf131087302
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 20:16:17 +0200

    modified:   client/src/Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import brainImage from './images/logo.svg'; // Import the image
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <img src={brainImage} alt="Logo" /> {/* Use the imported image */}
+      <img src={brainImage} alt="Logo" className="navbar-brand" style={{ maxHeight: '50px', marginRight: '10px' }} />
       </div>
     </nav>
   );
