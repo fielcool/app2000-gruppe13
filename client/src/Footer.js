677869b255283c0a33edf9e10493f92e8e8b739d
fielcool <115153391+fielcool@users.noreply.github.com>
2024-04-27 22:43:50 +0200

    modified:   client/src/App.css 	modified:   client/src/Footer.js

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <footer className="footer bg-dark text-white mt-4">
       <div className="container text-center py-3">
-        <input type="text" placeholder="Enter text here..." className="form-control" />
+        <p3> Denne nettsiden er ment som en tilleggsfunksjon for https://bigfive-test.com/ som del av Rubynors skolesamarbeid til app2000 for IT og informasjonssystemer. <br/>
+        MIT-license prosjekt ved Universitet i Sørøst-Norge.
+          </p3>
       </div>
     </footer>
   );
